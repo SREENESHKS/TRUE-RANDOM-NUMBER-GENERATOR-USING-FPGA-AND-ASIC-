884551ac741798f51e74832ce9c78da594e09a04
Sreenesh K S <sreeneshsreeshu2641@gmail.com>
2025-07-23 19:35:29 +0530

    Update VON_NEUMANN_DEBIASING.v

--- CODE/VON_NEUMANN_DEBIASING.v
+++ CODE/VON_NEUMANN_DEBIASING.v
@@ -8,16 +8,16 @@ module von_neumann (
     reg [1:0] buffer;
 
     always @(posedge clk) begin
-        buffer <= {buffer[0], in_bit};
+        buffer<={buffer[0], in_bit};
 
-        if (buffer == 2'b01) begin
-            out_bit <= 1;
-            valid <= 1;
-        end else if (buffer == 2'b10) begin
-            out_bit <= 0;
-            valid <= 1;
+        if (buffer==2'b01) begin
+            out_bit<=1;
+            valid<=1;
+        end else if (buffer==2'b10) begin
+            out_bit<=0;
+            valid<=1;
         end else begin
-            valid <= 0;
+            valid<=0;
         end
     end
 endmodule
